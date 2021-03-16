5f7c3ce03a4c47e6fe9937da966438438f2992b7
xavier <xmd404@pm.me>
2021-03-16 18:05:24 -0500

    style refactor

--- textMe.py
+++ textMe.py
@@ -1,8 +1,14 @@
 from twilio.rest import Client
-accountSID = 'AC467d3e051109fffee1ca2a3087e659b0'
-authToken = '32ef6fc7ab25273a662362825bc47576'
-twilioCli = Client(accountSID, authToken)
-myTwilioNumber = '+12313335367'
-myCellPhone = '+15129869718'
-messageBody = 'Hey Nika... im texting you from an automated python script :) - Xavier'
-message = twilioCli.messages.create(body=messageBody, from_=myTwilioNumber, to=myCellPhone)
+
+account_sid = 'AC467d3e051109fffee1ca2a3087e659b0'
+auth_token = '32ef6fc7ab25273a662362825bc47576'
+client = Client(account_sid, auth_token)
+twilio_number = '+12313335367'
+text_this_number = '+15129869718'
+message_body = 'Hey Nika... im texting you from an automated python script :) - Xavier'
+
+message = client.messages.create(
+    body=message_body,
+    from_=twilio_number,
+    to=text_this_number
+)
